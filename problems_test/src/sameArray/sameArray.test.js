9082f1b1764931ede578cbc8308e08deea7b2757
Khondaker97 <khondaker.tm@gmail.com>
2022-12-29 14:41:21 +0600

    restructure

--- problems_test/src/sameArray/sameArray.test.js
+++ problems_test/src/sameArray/sameArray.test.js
@@ -8,10 +8,10 @@ describe("Same Array", function () {
   it("should check same array length", function () {
     assert.isBoolean(sameArray([2, 3, 6], [3, 4]), false);
   });
-  it("2nd array should have squred of 1st array of values", function () {
+  it("2nd array should have squred of values of 1st array", function () {
     assert.isBoolean(sameArray([2, 3, 6], [3, 9, 36]), false);
   });
-  it("2nd array should have squred of 1st array of values", function () {
+  it("2nd array should have squred of values of 1st array", function () {
     assert.isBoolean(sameArray([2, 3, 6], [4, 9, 36]), true);
   });
   it("checks mixed array", function () {
