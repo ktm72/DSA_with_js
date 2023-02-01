b1f41563af705c79165f5b8c4a9e56a2e7a4ae58
Khondaker97 <khondaker.tm@gmail.com>
2023-02-01 19:52:05 +0600

    greatest common divisor of strings

---
+++ random_problems/gcdStrings.ts
@@ -0,0 +1,18 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+function gcdOfStrings(str1: string, str2: string): string {
+  if (str1 + str2 !== str2 + str1) return "";
+  const n: number = str1.length;
+  const m: number = str2.length;
+
+  const gcd = (x: number, y: number): number => {
+    if (!y) return x;
+    return gcd(y, x % y);
+  };
+  const div: number = gcd(n, m);
+  return str1.slice(0, div);
+}
+console.log(gcdOfStrings("ABCABC", "ABC")); //ABC
